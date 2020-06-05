3941f28b002e42d7d25dbafdc79d5020207f2712
Yao Lu <luyaocns@gmail.com>
2020-06-05 12:48:27 +0800

    init commit

--- src/GradChecker.jl
+++ src/GradChecker.jl
@@ -1,5 +1,28 @@
 module GradChecker
 
-# Write your package code here.
+export ngradient, gradcheck
+
+using LinearAlgebra
+
+function ngradient(f, x, is = 1:length(x); δ = 1e-2)
+    Δ = zero(x)
+    for i in is
+        tmp = x[i]
+        x[i] = tmp - δ / 2
+        y1 = f(x)
+        x[i] = tmp + δ / 2
+        y2 = f(x)
+        x[i] = tmp
+        Δ[i] = (y2 - y1) / δ
+    end
+    return Δ
+end
+
+function gradcheck(f, θ, is = 1:length(θ))
+    g = f'(θ)[is]
+    g′ = ngradient(f, θ, is)[is]
+    err = norm(g .- g′) / norm(g)
+    return g, g′, err
+end
 
 end
